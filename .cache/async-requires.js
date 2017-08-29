84c6f5178890e949dbec9cbb002de98b3907e803
Jose Jordan <jose.jordan@haelthtech.com>
2017-08-29 18:55:05 +0800

    Included .cache/ folder in commit

---
+++ .cache/async-requires.js
@@ -0,0 +1,29 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/home/joboy/workspace/meteor/joboyx-site/src/pages/404.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/home/joboy/workspace/meteor/joboyx-site/src/pages/index.js"),
+  "component---src-pages-page-0-js": require("gatsby-module-loader?name=component---src-pages-page-0-js!/home/joboy/workspace/meteor/joboyx-site/src/pages/page-0.js"),
+  "component---src-pages-page-1-js": require("gatsby-module-loader?name=component---src-pages-page-1-js!/home/joboy/workspace/meteor/joboyx-site/src/pages/page-1.js"),
+  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/home/joboy/workspace/meteor/joboyx-site/src/pages/page-2.js")
+}
+
+exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/joboy/workspace/meteor/joboyx-site/.cache/json/layout-index.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/home/joboy/workspace/meteor/joboyx-site/.cache/json/404.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/joboy/workspace/meteor/joboyx-site/.cache/json/layout-index.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/home/joboy/workspace/meteor/joboyx-site/.cache/json/index.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/joboy/workspace/meteor/joboyx-site/.cache/json/layout-index.json"),
+  "page-0.json": require("gatsby-module-loader?name=path---page-0!/home/joboy/workspace/meteor/joboyx-site/.cache/json/page-0.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/joboy/workspace/meteor/joboyx-site/.cache/json/layout-index.json"),
+  "page-1.json": require("gatsby-module-loader?name=path---page-1!/home/joboy/workspace/meteor/joboyx-site/.cache/json/page-1.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/joboy/workspace/meteor/joboyx-site/.cache/json/layout-index.json"),
+  "page-2.json": require("gatsby-module-loader?name=path---page-2!/home/joboy/workspace/meteor/joboyx-site/.cache/json/page-2.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/joboy/workspace/meteor/joboyx-site/.cache/json/layout-index.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/home/joboy/workspace/meteor/joboyx-site/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "component---src-layouts-index-js": require("gatsby-module-loader?name=component---src-layouts-index-js!/home/joboy/workspace/meteor/joboyx-site/.cache/layouts/index.js")
+}
---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/home/joboy/workspace/meteor/joboyx-site/src/layouts/index.js"
+  import data from "/home/joboy/workspace/meteor/joboyx-site/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
