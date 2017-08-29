4bda0660f4a2d4b3105fc777812635a9482c118f
Jose Jordan <jose.jordan@haelthtech.com>
2017-08-29 19:24:52 +0800

    Test netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () =>
   <div>
-    <h1>My Projects</h1>
+    <h1>My Projects!</h1>
     <h3>
         <a target="_kdb" href="https://workflowy.com/s/EKk5.JiAByIXPtu">Knowledge Database (WorkFlowy)</a>
     </h3>
